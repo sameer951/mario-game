d13a9eae44eea0577cc1f4dfb95e342df8927dec
Jyotikant <sanjay.row.com@gmail.com>
2020-09-14 23:16:49 +0530

    mamrio game build

---
+++ build/precache-manifest.40071226b571456401281551f5637b7d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707ea00de4bc10fa4a847ffda6f0a6ea",
+    "url": "/mario-game/index.html"
+  },
+  {
+    "revision": "6ed2e2dc0c0bc4a1579e",
+    "url": "/mario-game/static/css/main.774b5ea4.chunk.css"
+  },
+  {
+    "revision": "f13a04bfe38fa7a40590",
+    "url": "/mario-game/static/js/2.c3451aa3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mario-game/static/js/2.c3451aa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ed2e2dc0c0bc4a1579e",
+    "url": "/mario-game/static/js/main.1fd43575.chunk.js"
+  },
+  {
+    "revision": "e036e0019c171adbec6d",
+    "url": "/mario-game/static/js/runtime-main.985d8748.js"
+  }
+]);
